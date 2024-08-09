0df652111a506a07ae4cd532f5f9b4eabec252ea
cesarbravoz <118273662+cesarbravoz@users.noreply.github.com>
2024-08-09 15:22:14 -0500

    actualizacion 9 agosto 24

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const menu= document.querySelector(".menu");
+const navbar =document.querySelector(".navbar");
+
+menu.addEventListener("click",()=>{
+    navbar.classList.toggle("change");
+    menu.classList.toggle("change");
+    
+})
