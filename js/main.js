f3d72c16c6a06abb1d6eae29b4a708bbf6723edb
cesarbravoz <118273662+cesarbravoz@users.noreply.github.com>
2025-06-05 12:40:40 -0500

    actualizacion

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
 
-const menu= document.querySelector(".menu");
-const navbar =document.querySelector(".navbar");
 
-menu.addEventListener("click",()=>{
-    navbar.classList.toggle("change");
-    menu.classList.toggle("change");
-    
-})
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.slider-img');
+    const prevBtn = document.querySelector('.slider-btn.prev');
+    const nextBtn = document.querySelector('.slider-btn.next');
+    let current = 0;
+
+    function showImage(index) {
+        images.forEach((img, i) => {
+            img.classList.toggle('active', i === index);
+        });
+    }
+
+    prevBtn.addEventListener('click', () => {
+        current = (current - 1 + images.length) % images.length;
+        showImage(current);
+    });
+
+    nextBtn.addEventListener('click', () => {
+        current = (current + 1) % images.length;
+        showImage(current);
+    });
+
+    showImage(current);
+});
